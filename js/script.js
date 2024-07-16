dc9f0b6da17fac6c1405d5ce048804b4ba9b38c5
Max Unanian <Zeptorqyiad99@gmail.com>
2024-07-16 22:20:41 +0500

    Finished responsive burger menu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ let typed = new Typed('.auto-type', {
    backSpeed: 50,
    loop: true,
 })
+
+const headerMenu = document.querySelector('.header__budger')
+const burgerMenu = document.querySelector('.burger-list')
+const burgerLine1 = document.querySelector('.line-1')
+const burgerLine2 = document.querySelector('.line-2')
+const burgerLine3 = document.querySelector('.line-3')
+
+headerMenu.addEventListener('click', () => {
+   if (headerMenu) {
+      burgerMenu.classList.toggle('active')
+      burgerLine1.classList.toggle('active')
+      burgerLine2.classList.toggle('active')
+      burgerLine3.classList.toggle('active')
+      document.body.classList.toggle('_lock')
+   }
+})
